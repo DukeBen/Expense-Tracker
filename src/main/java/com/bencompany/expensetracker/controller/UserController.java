399f6de7bb4be4fbdd1c0cd7daafd3861c772afc
Ben Goldfried <2goldenfried@gmail.com>
2025-07-03 21:06:58 -0400

    Add createUser functionality and create UserRepository

--- src/main/java/com/bencompany/expensetracker/controller/UserController.java
+++ src/main/java/com/bencompany/expensetracker/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.bencompany.expensetracker.controller;
 
 import com.bencompany.expensetracker.model.User;
+import com.bencompany.expensetracker.repository.UserRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,9 +13,18 @@ import java.net.URI;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+    private final UserRepository userRepository;
+    public UserController(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody User user){
+        User savedUser = userRepository.save(user);
 
+        URI location = URI.create("/users/" + savedUser.getId());
+        return ResponseEntity.created(location).body(savedUser);
 
     }
 }
